1a132f2a00bab711f96f3acbfebb67627d87fb5d
Sercan Yücetaş <syucetas15973@gmail.com>
2026-06-05 20:31:19 +0300

    fixed the .gitignore file and sampleAudios.js should push now

---
+++ frontend/app/src/data/sampleAudios.js
@@ -0,0 +1,50 @@
+export const SAMPLE_AUDIOS = [
+  {
+    id: 'best-pesq-1',
+    label: 'Best PESQ #1',
+    filename: 'best_pesq_1_p257_344.wav',
+    path: '/samples/best_pesq_1_p257_344.wav',
+  },
+  {
+    id: 'best-pesq-2',
+    label: 'Best PESQ #2',
+    filename: 'best_pesq_2_p257_036.wav',
+    path: '/samples/best_pesq_2_p257_036.wav',
+  },
+  {
+    id: 'worst-pesq-1',
+    label: 'Worst PESQ #1',
+    filename: 'worst_pesq_1_p257_317.wav',
+    path: '/samples/worst_pesq_1_p257_317.wav',
+  },
+  {
+    id: 'worst-pesq-2',
+    label: 'Worst PESQ #2',
+    filename: 'worst_pesq_2_p257_056.wav',
+    path: '/samples/worst_pesq_2_p257_056.wav',
+  },
+  {
+    id: 'best-snr-1',
+    label: 'Best SNR #1',
+    filename: 'best_snr_1_p257_199.wav',
+    path: '/samples/best_snr_1_p257_199.wav',
+  },
+  {
+    id: 'best-snr-2',
+    label: 'Best SNR #2',
+    filename: 'best_snr_2_p257_287.wav',
+    path: '/samples/best_snr_2_p257_287.wav',
+  },
+  {
+    id: 'worst-snr-1',
+    label: 'Worst SNR #1',
+    filename: 'worst_snr_1_p257_376.wav',
+    path: '/samples/worst_snr_1_p257_376.wav',
+  },
+  {
+    id: 'worst-snr-2',
+    label: 'Worst SNR #2',
+    filename: 'worst_snr_2_p232_089.wav',
+    path: '/samples/worst_snr_2_p232_089.wav',
+  },
+]
